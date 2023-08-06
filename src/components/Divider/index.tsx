97370f37d2f2639887e31d32ff3818cf84868e6b
lucas-m12 <lucasmarinhodasilva2@gmail.com>
2023-08-06 17:18:58 -0300

    create divider component

---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import * as Separtor from "@radix-ui/react-separator";
+
+export const Divider = (props: Separtor.SeparatorProps) => {
+  return (
+    <Separtor.Root {...props} className="h-6 w-[1px] bg-gray-100 dark:bg-gray-dark-100" />
+  )
+}
